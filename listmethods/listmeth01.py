efc66a9d5d3c0b9f3da007a9b867a150f2666ae3
Adrian <38467013+AdrianLoree@users.noreply.github.com>
2021-07-19 18:07:41 +0000

    manipulating lists

---
+++ listmethods/listmeth01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''list of object and method lab'''
+
+proto = ["ssh", "http", "https"]
+print(proto)
+print(proto[1])
+proto.extend("dns") # this line will ad d, n, and s
+print(proto)
