d5953b0fc4c4734540e26ec8c6d7df695898f6f0
Adrian <38467013+AdrianLoree@users.noreply.github.com>
2021-07-20 14:28:07 +0000

    renaming and moving files

--- moveplease01.py
+++ moveplease01.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+'''A simple script to move two files into another folder'''
 
-# import libraries
+# import standard libraries
 import shutil
 import os
 
-# makr start position for script
-os.chdir('/home/student/mycode/')
+def main():
+    '''called at runtime'''
+    #mark start position for script
+    os.chdir('/home/student/mycode/')
+    
+    #move file/folder to selected location
+    shutil.move('raynor.obj', 'ceph_storage/')
+    
+    #naming prompt to not overwrite a file with a move method
+    xname = input('What is the new name for kerrigan.obj? ')
+    shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
 
-#move file/folder to selected location
-shutil.move('raynor.obj', 'ceph_storage/')
-
-#naming prompt to not overwrite a file with a move method
-xname = input('What is the new name for kerrigan.obj? ')
-shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
-
+main()
