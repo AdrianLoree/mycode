76c47d8730b58a9239c40b0e5e7791c0ba924f26
Adrian <38467013+AdrianLoree@users.noreply.github.com>
2021-07-20 15:14:07 +0000

    game challenege

--- challenge01.py
+++ challenge01.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''challenege to create a guessing game with multiple choice questsions By Adrian'''
 
-guess= input("What is Bruce Waynes secret identity?")
+guess= input("What is Bruce Wayne's secret identity?")
 
 if guess.lower() == "batman":
-    print("Correct Batman is Bruce Waynes secret identity! Congrats!")
+    print("Correct Batman is Bruce Wayne's secret identity! Congrats!")
 else:
     print("Sorry that is not correct... :(")
 
