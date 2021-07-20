433de951685303c0c66fe018b36f07e42eac1795
Adrian <38467013+AdrianLoree@users.noreply.github.com>
2021-07-20 15:19:15 +0000

    adding best practices to game challenge

--- challenge01.py
+++ challenge01.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 '''challenege to create a guessing game with multiple choice questsions By Adrian'''
 
-guess= input("What is Bruce Wayne's secret identity?")
+#script function
+def main()
+    '''loads at runtime'''
 
-if guess.lower() == "batman":
-    print("Correct Batman is Bruce Wayne's secret identity! Congrats!")
-else:
-    print("Sorry that is not correct... :(")
+#user input to answer the question
+    guess= input("What is Bruce Wayne's secret identity?")
 
+#compare the guess to the correct answer and converts all text to lower so it will always match
+    if guess.lower() == "batman":
+        #tell the user that they got the right answer
+        print("Correct Batman is Bruce Wayne's secret identity! Congrats!")
+   
+   #if the answer is the expected result tell them they did not get the question correct
+    else:
+         print("Sorry that is not correct... :(")
+
+#runs the script        
+main()
