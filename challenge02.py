c14c5f853f77830dab5f85265d8937a9121199f4
aloree <38467013+AdrianLoree@users.noreply.github.com>
2021-07-21 16:25:33 +0000

    api challenge

--- challenge02.py
+++ challenge02.py
@@ -2,15 +2,20 @@
 '''json challenge 2 july 21'''
 #import standard library
 import requests
-import json
+
 #API call to make
 ASTROS = "http://api.open-notify.org/astros.json"
 
-#request to get api call
-resp = requests.get(ASTROS)
+def main():
+    '''Pull in the astro api and tell who is the astronaut that has been in space the longest'''
+    #request to get api call
+    resp = requests.get(ASTROS)
 
-#print out json response
-json_from_astro = resp.json()
-print(json_from_astro)
-print(json_from_astro.get("people")[0].get("name") + " is the astronaut who currently has been in space the longest and they are on the " + json_from_astro.get("people")[0].get("craft"))
+    #print out json response
+    json_from_astro = resp.json()
+    print(json_from_astro)
+    
+    #print out who the astronaut who has been in space the longest and what craft they are on
+    print(json_from_astro.get("people")[0].get("name") + " is the astronaut who currently has been in space the longest and they are on the " + json_from_astro.get("people")[0].get("craft"))
 
+main()
