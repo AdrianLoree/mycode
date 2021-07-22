d32a32ebfc12130afe6a28bedbc5efe6951bf68b
Adrian Loree <38467013+AdrianLoree@users.noreply.github.com>
2021-07-22 18:47:00 +0000

    mtg sdk

--- magicsdk.py
+++ magicsdk.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 from mtgsdk import Card
+from mtgsdk import Set
+cardfind = input("What set do you wish to see ? ie 4ed ")
 
-card = Card.find(386616)
-cards = Card.where(set='ktk').where(subtypes='warrior,human').all()
+sets = Set.find(f'{cardfind}')
+print(sets.name, sets.code)
+cards = Card.where(set_name=f'{sets.name}')
 
-print(card)
 print(cards)
 
