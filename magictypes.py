2baba81dbb8b258af19f4f7a89ed2f9c89689a9a
Adrian Loree <38467013+AdrianLoree@users.noreply.github.com>
2021-07-22 17:37:40 +0000

    magic

--- magictypes.py
+++ magictypes.py
@@ -7,22 +7,25 @@ import json
 API = "https://api.magicthegathering.io/v1/"
 
 def main():
-    
+    #print purpse of script to user
     print('Select a card type from the below:')
-
+    #api request
     resp = requests.get(f"{API}types")
-
+    #api response to display options to user
     ctypes = resp.json()
-
+    #display options
     print(ctypes)
-
+    # take user input
     types = input("What type of cards do you wish to see? (example Land, Artifact, etc.) ").capitalize()
-   
+    #send url request to API
     resptypes = requests.get(f"{API}cards?type={types}")
-#    card = resptypes.json()
 
-#    print(card)
-    
+    #used to show resonse was working
+    #    card = resptypes.json()
+
+    #    print(card)
+
+    #change json response to text file?
     cards = resptypes.text
     
     #load the json file
